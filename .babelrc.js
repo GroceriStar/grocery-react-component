a5218aa3193233587597cbea4e9ef4451aa2ed14
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-11 05:35:56 +0200

    i'm debuggin

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const presets = [];
+const plugins = [];
+
+module.exports = { presets, plugins };
+
+// "babel-plugin-module-resolver": "^3.1.1",
+// npx babel src --out-dir lib --ignore "src/**/*.spec.js","src/**/*.test.js"
