3cb2c52d3bd86cba7fae54fabecddff51a80f9ae
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-11 05:54:46 +0200

    jest still not working. it can affect to build...

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const presets = [];
-const plugins = [];
+const path = require('path');
+
+const presets = [
+  "@babel/preset-env",
+  "@babel/preset-react"
+];
+
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ["./src/"],
+      alias: {
+        "~": "./src/components"
+      }
+    }
+    // require.resolve('@babel/plugin-module-resolver')
+  ]
+
+];
 
 module.exports = { presets, plugins };
 
 // "babel-plugin-module-resolver": "^3.1.1",
 // npx babel src --out-dir lib --ignore "src/**/*.spec.js","src/**/*.test.js"
+// https://gist.github.com/nodkz/41e189ff22325a27fe6a5ca81df2cb91
--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,15 +1,17 @@
 import React    from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, {shallow, configure } from 'enzyme';
+// import ReactDOM from 'react-dom';
+import Enzyme, { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import List from '~/List/List';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<List />', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<List />, div);
-    });
+describe("<DisplayList /> component shallow only methods ", () => {
+  it("renders without crashing", () => {
+
+    const wrapper = shallow(<List />);
+    expect(wrapper).toBe(true)
+      // ReactDOM.render(<List />, div);
+  });
 });
