26efdc938e8e495d3ad82ea3950a06e9bbe3f48a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-25 11:18:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import TodaysDate     from '~/TodaysDate'
 import List           from '~/List/List'
 import DefaultList    from '~/List/DefaultList'
+import ListF           from '~/List/ListF'
 
 import IngredientList from '~/IngredientList/IngredientList'
 import DepartmentList from '~/DepartmentList/DepartmentList'
@@ -10,6 +11,7 @@ export {
   TodaysDate,
   List,
   DefaultList,
+  ListF,
   
   IngredientList,
   DepartmentList,
